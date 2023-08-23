3c1d11efb34c9f174458b9f0cc8b1b430b3f632b
alflem <albin.flemstrom@xlent.se>
2023-08-23 15:39:50 +0200

    ContentTest

--- src/app/components/petal/petal.component.ts
+++ src/app/components/petal/petal.component.ts
@@ -28,7 +28,7 @@ export class PetalComponent {
   petals: Petal[] = [
     {
       imageAvatarUrl: 'https://www.w3schools.com/howto/img_avatar.png',
-      title: 'Anställning och admin',
+      title: 'Välkommen',
       subtitle: 'Veronica Andersson',
       description: 'Vår VD är Veronica, det är hon som har koll på vad som behöver vara på plats i och med din anställningsstart',
       imageUrl: 'https://www.w3schools.com/howto/img_avatar.png',
@@ -37,44 +37,53 @@ export class PetalComponent {
     },
     {
       imageAvatarUrl: 'https://th.bing.com/th/id/OIG.2cFXsriBPnvyLfdE5jag?pid=ImgGn',
-      title: 'Blomblad 2',
-      description: 'Beskrivning för blomblad 2',
+      title: 'Buddy/coach',
+      description: 'Du har säkert redan träffat din Buddy, som är den som håller ihop din intro och har lite extra koll på att du får svar på dina frågor och funderingar. Sen efter några månader när du kommit igång, så kommer du få möjlighet att själv önska en coach.  ',
       imageUrl: 'https://th.bing.com/th/id/OIG.2cFXsriBPnvyLfdE5jag?pid=ImgGn',
-      urlNamn: 'url2',
+      urlNamn: 'Buddy/coach',
       url: 'http://www.url2.com'
     },
     {
-      imageAvatarUrl: 'data.jpg', 
-      title: 'Blomblad 3',
+      imageAvatarUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn', 
+      title: 'Startklar',
       description: 'Beskrivning för blomblad 2',
-      imageUrl: 'data.jpg',
+      imageUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn',
       urlNamn: 'Startklar',
       url: 'http://www.url2.com'
     },
     {
-      title: 'Blomblad 4',
+      title: 'Anställning och admin',
       description: 'Beskrivning för blomblad 2',
-      imageUrl: 'bild2.jpg',
+      imageUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn'
+      urlNamn: 'Anställning och admin',
       url: 'http://www.url2.com'
     },
     {
+      title: 'Digital setup',
       description: 'Beskrivning för blomblad 2',
-      imageUrl: 'bild2.jpg',
+      imageUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn',
+      urlNamn: 'Digital setup',
       url: 'http://www.url2.com'
     },
     {
-      description: 'Beskrivning för blomblad 2',
-      imageUrl: 'bild2.jpg',
+      title: 'Konsultrollen ',
+      description: 'Konsultrollen',
+      imageUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn',
+      urlNamn: 'Startklar',
       url: 'http://www.url2.com'
     },
     {
+      title: 'Konsultrollen ',
       description: 'Beskrivning för blomblad 2',
-      imageUrl: 'bild2.jpg',
+      imageUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn',
+      urlNamn: 'Startklar',
       url: 'http://www.url2.com'
     },
     {
+      title: 'Konsultrollen',
       description: 'Beskrivning för blomblad 2',
-      imageUrl: 'bild2.jpg',
+      imageUrl: 'https://th.bing.com/th/id/OIG.VBrZKMr3FOvzlq5Zlewt?pid=ImgGn',
+      urlNamn: 'Startklar',
       url: 'http://www.url2.com'
     },
     
